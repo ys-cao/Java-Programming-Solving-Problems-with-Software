f63922278c229d143ffc961869a70e8fc2126288
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-02 13:23:26 -0500

    Create Part2.java

---
+++ Week2/StringsFirstAssignment/Part2.java
@@ -0,0 +1,69 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+import java.io.*;
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+import java.io.*;
+
+public class Part2 {
+    public String findSimpleGene(String dna, String startCodon, String stopCodon) {
+        int startIndex = dna.toUpperCase().indexOf(startCodon);
+        int stopIndex = dna.toUpperCase().indexOf(stopCodon, startIndex + 3);
+        
+        String result = "";
+        
+        if (startIndex == -1 || stopIndex == -1) return result;
+        if ((stopIndex - startIndex) % 3 == 0) {
+            result = dna.substring(startIndex, stopIndex + 3);
+        }
+        return result;
+    }
+    
+    public void testSimpleGene() {
+        String startCodon = "ATG";
+        String stopCodon = "TAA";
+        
+        // DNA without ATG
+        String dna = "CGTAGCAATTATAA";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+        
+        // DNA without TAA
+        dna = "CATGGCAATTATTA";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+        
+        // DNA without ATG and TAA
+        dna = "CGTAGCAATTATCA";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+        
+        // DNA with ATG and TAA but substring is not a multiple of 3.
+        dna = "CGATGCAATATAA";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+        
+        // DNA with ATG and TAA but substring iS a multiple of 3.
+        dna = "CGATGCAATTATAA";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+        
+        dna = "cgatgcaattataa";
+        System.out.println("DNA strand is " + dna);
+        System.out.println("Gene is " + findSimpleGene(dna, startCodon, stopCodon));
+    }
+}
