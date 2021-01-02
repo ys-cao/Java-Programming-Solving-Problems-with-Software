a32dab5c0361d132d34a605f50744651d533a687
Yishuai Cao <74084410+ys-cao@users.noreply.github.com>
2021-01-02 14:53:13 -0500

    Create Part4.java

---
+++ Week2/StringsFirstAssignment/Part4.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of Part4 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+import java.io.*;
+
+public class Part4 {
+    public void findURL() {
+        String key = "youtube.com";
+        String doubleQuote = "\"";
+        URLResource url = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        
+        for (String word : url.words()) {
+            String wordLowerCase = word.toLowerCase();
+            int location = wordLowerCase.indexOf(key);
+            if (location != -1) {
+                int startIndex = wordLowerCase.lastIndexOf(doubleQuote, location);
+                int stopIndex = wordLowerCase.indexOf(doubleQuote, location);
+                String address = word.substring(startIndex, stopIndex + 1);
+                System.out.println(address);
+            }
+        }
+    }
+}
